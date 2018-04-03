3d4969260e6cc40ce663071c3ba8f7a94ece5b4b
SUPBRABHA <38032795+SUPBRABHA@users.noreply.github.com>
2018-04-03 15:18:28 +0530

    Update and rename lar.c to oe.c

---
+++ oe.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int number=4;
+  if(number%2==0)
+    printf("given number is even");
+  else
+    printf("given number is odd");
+  getch();
+}
