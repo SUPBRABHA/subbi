377195cfee5b56a53c4b5d1f2238b85a85a66488
SUPBRABHA <38032795+SUPBRABHA@users.noreply.github.com>
2018-04-03 14:46:35 +0530

    Update and rename num.c to lar.c

---
+++ lar.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int a=4;
+  int b=5;
+  int c=6;
+  if(a>b&&a>c)
+  {
+  printf("4");
+  }
+  else if(b>a&&b>c)
+  {
+    printf("5");
+  }
+  else
+    printf("6");
+  getch();
+}
