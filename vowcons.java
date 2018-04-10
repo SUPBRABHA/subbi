c8159842e461c33633ff0259d8d1853731b55e06
SUPBRABHA <38032795+SUPBRABHA@users.noreply.github.com>
2018-04-10 20:40:01 +0530

    Create vowcons.java

---
+++ vowcons.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+class vowcons
+{
+public static void main(String args[])
+{
+char c='*';
+if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u')
+System.out.println(c +"is an vowel");
+else
+System.out.println(c +"is a consonant");
+}
+}
