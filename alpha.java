8c72178afa149d8f3cbf469f7a2ea40c83e5ef27
SUPBRABHA <38032795+SUPBRABHA@users.noreply.github.com>
2018-04-10 20:17:11 +0530

    Create alpha.java

---
+++ alpha.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class alpha
+{
+public static void main(String args[])
+{
+char c='*';
+if((c>='a'&&c<='z')||if(c>='A'&&c<='Z'))
+System.out.println(c+ "is an alphabet");
+else
+System.out.println(c +"is not an alphabet");
+}
+}
