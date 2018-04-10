b12c91d3bc1952d34d62e44c95b398d6398a15cb
SUPBRABHA <38032795+SUPBRABHA@users.noreply.github.com>
2018-04-10 20:49:58 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+class hello
+{
+public static void main(String args[])
+{
+BufferedReader br=new BufferedReader(new InputStreamReader(System.in);
+String s=br.readLine();
+int n=Integer.parseInt(s);
+for(int i=0;i<n;i++)
+System.out.println("hello");
+}
+}
