d53361fb30c5c581e24429ded9a445351fdf75fe
SUPBRABHA <38032795+SUPBRABHA@users.noreply.github.com>
2018-04-18 20:18:06 +0530

    Create power.java

---
+++ power.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class power
+{
+public static void main(String args[])
+{
+int base=4,exponent=5;
+double result=Math.pow(base,exponent);
+System.out.prinln("Answer=" +result);
+}
+}
